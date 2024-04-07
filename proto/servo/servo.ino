ad234454766bacbba2a2ae834ab8d7c3a8edccd0
thinkier <26728734+thinkier@users.noreply.github.com>
2024-04-07 15:37:45 +1000

    tested servo design & angles

---
+++ proto/servo/servo.ino
@@ -0,0 +1,29 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+#define USMIN  1600
+#define USMAX  2300
+#define SERVO_FREQ 50
+
+void setup() {
+  pwm.begin();
+  pwm.setOscillatorFrequency(27000000);
+  pwm.setPWMFreq(SERVO_FREQ);
+
+  delay(10);
+}
+
+void loop() {
+  for (uint16_t microsec = USMIN; microsec < USMAX; microsec++) {
+    pwm.writeMicroseconds(servonum, microsec);
+  }
+
+  delay(500);
+  for (uint16_t microsec = USMAX; microsec > USMIN; microsec--) {
+    pwm.writeMicroseconds(servonum, microsec);
+  }
+
+  delay(500);
+}
