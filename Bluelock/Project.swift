16c855ea573367953f8ed5afa5e6bde6dc547e95
thinkier <26728734+thinkier@users.noreply.github.com>
2024-04-21 00:29:42 +1000

    successfully added SQLite.swift as a dep pkg

--- Bluelock/Project.swift
+++ Bluelock/Project.swift
@@ -2,20 +2,31 @@ import ProjectDescription
 
 let project = Project(
     name: "Bluelock",
+    organizationName: "thinkier.github.io",
+    packages: [
+        .remote(url: "https://github.com/stephencelis/SQLite.swift", requirement: .upToNextMajor(from: "0.15"))
+    ],
     targets: [
         .target(
             name: "Bluelock",
-            destinations: .iOS,
+            destinations: [.iPhone, .appleWatch],
             product: .app,
             bundleId: "io.github.thinkier.Bluelock",
+            deploymentTargets: .multiplatform(iOS: "16.0", watchOS: "9.0"),
             infoPlist: .extendingDefault(
                 with: [
                     "UILaunchStoryboardName": "LaunchScreen.storyboard",
+                    "NSUserNotificationsUsageDescription": "Recieve alerts when the lock is actuated automatically.",
+                    "UIBackgroundModes": ["bluetooth-central"],
+                    "UIFileSharingEnabled": true,
+                    "LSSupportsOpeningDocumentsInPlace": true
                 ]
             ),
             sources: ["Bluelock/Sources/**"],
             resources: ["Bluelock/Resources/**"],
-            dependencies: [.package(url: "https://github.com/stephencelis/SQLite.swift", from: "0.15.0")]
+            dependencies: [
+                .package(product: "SQLite.swift", type: .runtime)
+            ]
         )
     ]
 )
