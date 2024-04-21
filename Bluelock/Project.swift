1462ce6c822b1463fcad5777d1080dcd878975f8
thinkier <26728734+thinkier@users.noreply.github.com>
2024-04-21 12:55:19 +1000

    added all the necessary project configs w/o code

--- Bluelock/Project.swift
+++ Bluelock/Project.swift
@@ -3,14 +3,19 @@ import ProjectDescription
 let project = Project(
     name: "Bluelock",
     organizationName: "thinkier.github.io",
+    options: .options(xcodeProjectName: "Bluelock"),
     packages: [
         .remote(url: "https://github.com/stephencelis/SQLite.swift", requirement: .upToNextMajor(from: "0.15"))
     ],
+    settings: .settings(base: SettingsDictionary()
+        .automaticCodeSigning(devTeam: "6X8VAXGXBX")
+    ),
     targets: [
         .target(
             name: "Bluelock",
             destinations: [.iPhone, .appleWatch],
             product: .app,
+            productName: "Bluelock",
             bundleId: "io.github.thinkier.Bluelock",
             deploymentTargets: .multiplatform(iOS: "16.0", watchOS: "9.0"),
             infoPlist: .extendingDefault(
@@ -24,9 +29,8 @@ let project = Project(
             ),
             sources: ["Bluelock/Sources/**"],
             resources: ["Bluelock/Resources/**"],
-            dependencies: [
-                .package(product: "SQLite.swift", type: .runtime)
-            ]
+            entitlements: "Bluelock/Bluelock.entitlements",
+            dependencies: []
         )
     ]
 )
